4fb8c92b76ac34ce9bbfbf5e3ac2dae38a4b089e
RODRIGUEZ CASTELLANOS ERICK YAMIL <castellanosyamil@gmail.com>
2026-02-20 12:40:48 -0600

    Actualizando Helloworld.cs con un bucle: for

--- Helloworld.cs
+++ Helloworld.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hola Mundo");
+        for (int i=0; i < 5; i++)
+        {
+            Console.WriteLine("Hola Mundo");
+        }
     }
 }
